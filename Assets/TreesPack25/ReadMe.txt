e69007e32381247a71a3339c6f7e3e4379755ecb
Ken Marold <kenmarold@gmail.com>
2026-03-30 10:01:23 -0500

    Week 10 Terrain Example Added

---
+++ Assets/TreesPack25/ReadMe.txt
@@ -0,0 +1,17 @@
+============== Trees Pack 2025 (HQ) ==============
+Pack of trees:
+1. Pine
+2. Birch
+3. Fir tree
+4. Maple
+5. Oak
+
+Trees designed for desktops and consoles.
+
+
+IL.ranch, 2025.
+ILonion32@gmail.com
+
+www.ilonion.com
+
+
---
+++ Assets/TreesPack25/Script/TrWind.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//IL.ranch, 2025. ILonion32@gmail.com
+namespace ILranch
+{
+    public class TrWind : MonoBehaviour
+    {
+        [System.Serializable]
+        public class WindValues
+        {
+            public Material SharedMaterial;
+            public float Power;
+            [HideInInspector]
+            public float Scatter;
+        }
+        [Header("Simple wind")]
+        public WindValues[] _WindValues;
+
+        void Awake()
+        {
+            UnityEngine.Random.InitState((int)System.DateTime.Now.Ticks * 1000);
+        }
+
+        void Start()
+        {
+            for (int k0 = 0; k0 < _WindValues.Length; k0++)
+            {
+                _WindValues[k0].Scatter = UnityEngine.Random.Range(0.1f, 4f);
+            }
+        }
+
+        void FixedUpdate()
+        {
+            for (int k0 = 0; k0 < _WindValues.Length; k0++)
+            {
+                _WindValues[k0].SharedMaterial.SetFloat("_HeightAmplitude", _WindValues[k0].Power * Mathf.Sin(Time.time * _WindValues[k0].Scatter));
+            }
+        }
+    }
+}
